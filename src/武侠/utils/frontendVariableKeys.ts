ca464463aba83fe1c652a4877bb6dd039a2f01e5
jam4435 <jamsamzi49@gmail.com>
2026-09-25 01:20:27 +0800

    wuxia: register current location lore derived key

--- src/武侠/utils/frontendVariableKeys.ts
+++ src/武侠/utils/frontendVariableKeys.ts
@@ -1,5 +1,6 @@
 export const FRONTEND_VARIABLES_KEY = '前端变量';
 export const FRONTEND_BATTLE_ZONE_KEY = '战力区';
+export const FRONTEND_LOCATION_LORE_KEY = '当前地点信息';
 export const FRONTEND_RANDOM_NUMBERS_KEY = '随机数';
 export const FRONTEND_CULTIVATION_REFERENCE_KEY = '修为变化参考';
 export const FRONTEND_MERIDIANS_KEY = '奇经八脉';
